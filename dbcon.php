5f5f690ac6e9e884c27422e903709d7a4aba862b
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-20 12:31:54 +0400

    first commit

---
+++ dbcon.php
@@ -0,0 +1,17 @@
+<?php 
+
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "crud_operations");
+
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+    if(!$connection){
+        die("Connection Failed");
+    }
+    else{
+        echo "yes";
+    }
+
+?>
