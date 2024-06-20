c85a638203c2ed3dc990077fbdf48c545006011b
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-20 13:44:39 +0400

    style: add student modal

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
     <title>Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <h1 class="" id="main_title">Crud application in PHP</h1>
--- index.php
+++ index.php
@@ -1,38 +1,72 @@
 <?php include('header.php'); ?>
 <?php include('dbcon.php'); ?>
 
-        <h2>All Students</h2>
-    <table class="table table-hover table-bordered table-striped">
-        <thread>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Age</th>
-            </tr>
-        </thread>
-        <tbody>
-            <?php 
-                $query = "select * from `students`";
-                $result = mysqli_query($connection, $query);
-                if(!$result){
-                    die("query Failed".mysqli_error());
-                }
-                else{
-                    while($row = mysqli_fetch_assoc($result)){
-                        ?>
-                            <tr>
-                                <td><?php echo $row["id"]; ?></td>
-                                <td><?php echo $row["first_name"]; ?></td>
-                                <td><?php echo $row["last_name"]; ?></td>
-                                <td><?php echo $row["age"]; ?></td>
+<div class="box1">
+    <h2>All Students</h2>
+    <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Student</button>
+</div>
 
-                            </tr>
-                        <?php
-                    }
+<table class="table table-hover table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+            $query = "SELECT * FROM `students`";
+            $result = mysqli_query($connection, $query);
+            if (!$result) {
+                die("Query Failed: " . mysqli_error($connection));
+            } else {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                    <tr>
+                        <td><?php echo $row["id"]; ?></td>
+                        <td><?php echo $row["first_name"]; ?></td>
+                        <td><?php echo $row["last_name"]; ?></td>
+                        <td><?php echo $row["age"]; ?></td>
+                    </tr>
+                    <?php
                 }
+            }
+        ?>
+    </tbody>
+</table>
+<form >
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Student</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <div class="form-group">
+            <label for="f_name">First Name</label>
+            <input type="text" class="form-control" name="f_name">
+          </div>
+          <div class="form-group">
+            <label for="l_name">Last Name</label>
+            <input type="text" class="form-control" name="l_name">
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input type="text" class="form-control" name="age">
+          </div>
+      </div>
+      <div class="modal-footer">
+      <button type="submit" class="btn btn-success">Add</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
 
-            ?>
-        </tbody>
-    </table>
 <?php include('footer.php'); ?>
